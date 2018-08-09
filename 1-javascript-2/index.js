a69589dfd8056eeb57a2299f5eab16826a53c427
Marina Averyanova <averyanova.m.e@yandex.ru>
2018-08-10 00:41:38 +0300

    1-javascript-2

--- 1-javascript-2/index.js
+++ 1-javascript-2/index.js
@@ -1,41 +1,4 @@
 const fn1 = () => {
-<<<<<<< HEAD
-        return Promise.resolve(1);
-}
-
-const fn2 = () => new Promise(resolve => {
-  setTimeout(() => resolve(2), 3000);
-})
-
-const fn3 = () => new Promise(resolve => {
-  setTimeout(() => resolve(3), 1000);
-})
-
-const fn4 = () => new Promise(resolve => {
-  setTimeout(() => resolve(4), 1000);
-})
-
-function promiseReduce(asyncFunctions, reduce, initialValue) {
-  let acc = initialValue;
-  let counter = 0;
-
-  return new Promise(resolve => {
-    asyncFunctions.forEach(function(fn) {
-      let resultPromise = new Promise(function(resolve) {
-        resolve((async function(fn) {
-          return await fn();
-        }(fn)));
-      });
-
-      resultPromise.then(
-        function fulfilled(res) {
-          acc = reduce(acc, res);
-          counter++;
-          counter === asyncFunctions.length && resolve(acc);
-        }
-      );
-    });
-=======
   return Promise.resolve('t');
 }
 
@@ -60,26 +23,15 @@ function promiseReduce(asyncFunctions, reduce, initialValue) {
       });
     };
     iterator(0, initialValue);
->>>>>>> javascript-2
   });
 }
 
 var promiseReduceResult = promiseReduce([fn1, fn2, fn3, fn4], function (memo, value) {
-<<<<<<< HEAD
-  console.log('reduce');
-  return memo + value;
-}, 0);
-
-promiseReduceResult.then(
-  function fulfilled(res) {
-    console.log('promise reduce result', res, 'result is valid', res === 10);
-=======
   return memo + value;
 }, 's');
 
 promiseReduceResult.then(
   function fulfilled(res) {
     console.log('promise reduce result', res, 'result is valid', res === 'start');
->>>>>>> javascript-2
   }
 );
